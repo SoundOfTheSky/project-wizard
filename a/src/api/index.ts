bbb70671af4d8c80ead70262265a3e9a3ec27a09
SoundOfTheSky <tkachiov.2000@gmail.com>
2021-05-23 23:46:35 +0300

    stashed testing for now, electron is my best friend

--- a/src/api/index.ts
+++
@@ -1,4 +0,0 @@
-import { todoItem } from '@/store/slices/todos';
-
-export const getTodos = async (): Promise<todoItem[]> =>
-  (await (await fetch('https://jsonplaceholder.typicode.com/todos')).json()).slice(0, 4);
